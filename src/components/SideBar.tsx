3b7605f84b963bc18ba45cada59e85b774a046bd
MoisesMorandini <moises-morandini@hotmail.com>
2021-08-05 17:01:43 -0300

    Separation of Content and SideBar components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: Array<
+    {
+      id: number;
+      name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+      title: string;
+    }
+  >;
+  onClick: any;
+  selected: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onClick(genre.id)}
+            selected={props.selected === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
